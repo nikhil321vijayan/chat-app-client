297464e59edc4838b1cb646b9bb117e68d66328b
Nikhil Vijayan <nikhil321vijayan@gmail.com>
2022-11-28 15:02:15 -0500

    Added css to the App

--- src/Chat.js
+++ src/Chat.js
@@ -25,7 +25,7 @@ function Chat({socket, username, room}) {
     }, [socket]);
 
     return (
-        <div>
+        <div className='chat-window'>
             <div className='chat-header'>
                 <p>Live Chat</p>
             </div>
